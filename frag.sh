c70b8559bf46b0a2625e75f803502b70914e99f7
goggle1 <goggle1@163.com>
2013-08-09 17:37:55 +0800

    first commit

---
+++ frag.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+ROOT_DIR=$1
+LOG=$2
+
+function frag_file()
+{
+	local FILE
+	FILE=$1
+	filefrag ${FILE} >> $LOG
+}
+
+function list_dir()
+{	
+	local DIR
+	local FILE2
+	DIR=$1
+	for FILE2 in `ls -a ${DIR}`
+  	do
+		echo "${DIR}/${FILE2}"
+  		if [ x"${FILE2}" != x"." -a x"${FILE2}" != x".." ]; then
+			if [ -d "${DIR}/${FILE2}" ]; then
+				list_dir "${DIR}/${FILE2}"
+      			elif [ -f "${DIR}/${FILE2}" ]; then
+      				frag_file "${DIR}/${FILE2}"
+			fi
+		fi
+	done
+}
+
+if [ x"${ROOT_DIR}" = x"" ]; then
+	echo "please input DIR name"
+	exit 1
+fi
+
+if [ x"${LOG}" = x"" ]; then
+	echo "please input LOG name"
+	exit 1
+fi
+
+list_dir ${ROOT_DIR}
---
+++ one.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROOT_PATH=/home/guoqiang
+#${ROOT_PATH}/frag.sh /media1 ${ROOT_PATH}/frag.log
+${ROOT_PATH}/frag.sh /media2 ${ROOT_PATH}/frag.log
+${ROOT_PATH}/frag.sh /media3 ${ROOT_PATH}/frag.log
+${ROOT_PATH}/frag.sh /media4 ${ROOT_PATH}/frag.log
+${ROOT_PATH}/frag.sh /media5 ${ROOT_PATH}/frag.log
+${ROOT_PATH}/frag.sh /media6 ${ROOT_PATH}/frag.log
+${ROOT_PATH}/frag.sh /media7 ${ROOT_PATH}/frag.log
+${ROOT_PATH}/frag.sh /media8 ${ROOT_PATH}/frag.log
+${ROOT_PATH}/frag.sh /media9 ${ROOT_PATH}/frag.log
+${ROOT_PATH}/frag.sh /media10 ${ROOT_PATH}/frag.log
